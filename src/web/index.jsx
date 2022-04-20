e7a70d2cac737861ec23ca006d0a96ffc3e79a9c
peasee <98815791+peasee@users.noreply.github.com>
2022-04-20 11:18:36 +1000

    Reset the template react stuff I brought in from another project

--- src/web/index.jsx
+++ src/web/index.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { setChonkyDefaults } from 'chonky';
-import { ChonkyIconFA } from 'chonky-icon-fontawesome';
-setChonkyDefaults({ iconComponent: ChonkyIconFA });
-
 import App from "./app";
 
 ReactDOM.render(<App />, document.getElementById("main"));
---
+++ src/web/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+export default function Homepage(props) {
+    return (
+        <div>
+            <h1>Homepage</h1>
+        </div>
+    )
+}
