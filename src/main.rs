43358dcb9e64dfb492c34d1b19c5e1109949f0c4
peasee <98815791+peasee@users.noreply.github.com>
2024-03-30 10:21:57 +0000

    feat: Initialize a barebones Rust binary

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use anyhow::Result;
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let subscriber = FmtSubscriber::builder()
+        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
+        // will be written to stdout.
+        .with_max_level(Level::TRACE)
+        // completes the builder.
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    info!("Hello, world!");
+
+    Ok(())
+}
