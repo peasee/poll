fd11e8fff66695231037a19f04678713e54d5215
peasee <98815791+peasee@users.noreply.github.com>
2024-03-30 10:18:46 +0000

    refactor: Update deps, migrate frontend code to web/

--- webpack.config.js
+++ webpack.config.js
@@ -4,39 +4,39 @@ const config = require("./config.json");
 const webpack = require("webpack");
 
 module.exports = {
-    entry: './src/web/index.jsx',
-    output: {
-      path: __dirname + '/dist',
-      filename: "bundle.js"
-    },
-    resolve: {
-      extensions: ["", ".js", ".jsx"]
-    },
-    devServer: {
-      historyApiFallback: true
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
-        },
-        {
-          test: /\.(less|css)$/,
-          use: [
-            'style-loader',
-            'css-loader',
-            'less-loader',
-          ],
-        },
-      ]
-    },
-    devtool: "inline-source-map",
-    plugins: [
-      new webpack.DefinePlugin({
-        "API_HOST": `"${config.clientAPI}"`,
-        "RECAP_SITE_KEY": `"${config.recapSiteKey}"`
-      })
+  entry: './web/index.jsx',
+  output: {
+    path: __dirname + '/dist',
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(less|css)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader',
+        ],
+      },
     ]
-  };
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new webpack.DefinePlugin({
+      "API_HOST": `"${config.clientAPI}"`,
+      "RECAP_SITE_KEY": `"${config.recapSiteKey}"`
+    })
+  ]
+};
