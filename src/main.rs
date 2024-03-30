6f8239c622786d91d680142b6f3cecdf16d97395
peasee <98815791+peasee@users.noreply.github.com>
2024-03-30 11:00:04 +0000

    feat: Add base axum server with example routes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use anyhow::Result;
+use serde_json::{json, Value};
 use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
+
+use axum::{extract::Path, http::StatusCode, routing::get, Json, Router};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -15,5 +18,22 @@ async fn main() -> Result<()> {
 
     info!("Hello, world!");
 
+    // build our application with a route
+    let app = Router::new()
+        .route("/alive", get(alive))
+        .route("/alive/:name", get(alive_with_name));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3081").await?;
+    axum::serve(listener, app).await?;
+
     Ok(())
+}
+
+async fn alive() -> (StatusCode, Json<Value>) {
+    (StatusCode::OK, Json(json!({ "hello": "world" })))
+}
+
+async fn alive_with_name(Path(name): Path<String>) -> (StatusCode, Json<Value>) {
+    (StatusCode::OK, Json(json!({ "hello": name })))
 }
