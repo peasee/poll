f0b8876e2fa8c647ab79b1e354bb4706edc979d9
peasee <98815791+peasee@users.noreply.github.com>
2024-04-02 13:30:38 +0000

    docs: Update README.md

--- web/pages/Homepage.jsx
+++ web/pages/Homepage.jsx
@@ -6,7 +6,7 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
 export function Homepage(props) {
-    useEffect(()=>{
+    useEffect(() => {
         props.clearError();
     }, []);
 
@@ -15,7 +15,7 @@ export function Homepage(props) {
             <Row>
                 <Col xs={12} md={8} className="offset-0 offset-md-2">
                     <p>
-                        Don't care about the project and just want to make a poll? No problem. 
+                        Don't care about the project and just want to make a poll? No problem.
                         <Link to="/poll" className="btn btn-warning mt-2 p-2 d-block w-50 w-md-25">Create a new poll</Link>
                     </p>
                 </Col>
@@ -30,19 +30,15 @@ export function Homepage(props) {
                         Votes on polls are restricted to a single vote per-IP, and are secured with ReCaptcha to limit botted submissions.
                     </p>
                     <p>
-                        Benefits of this project being open-source is that if you don't like using the publicly provided instance at <a href="https://poll.nullabyte.com">https://poll.nullabyte.com</a>, you can host it yourself on your own infrastructure!
+                        Benefits of this project being open-source is that you can host it yourself on your own infrastructure!
                         Perfect for users with high-traffic polls that have the resources to pay for equally high-performance servers.
                     </p>
                     <p>
                         You can view the source code (including basic instructions on setup) <a href="https://github.com/peasee/poll">at the GitHub repo</a>.
                         You can also raise issues on the repo if you encounter any bugs.
                     </p>
-                    <p>
-                        The publicly provided instance does not run any advertisements nor analytics (excluding ReCaptcha) - the only thing you load is the tool.
-                        Donations are appreciated to support the running of this instance, but are not required! <a href="https://github.com/peasee/poll">View the details on the repo</a> for how to donate, if you're so inclined.
-                    </p>
                 </Col>
             </Row>
         </div>
-    )
+    );
 }
