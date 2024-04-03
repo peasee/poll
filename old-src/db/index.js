bfe80f67d25fec5c4e8b1f217fcaffe0671ffe2a
peasee <98815791+peasee@users.noreply.github.com>
2024-04-03 14:34:19 +0000

    chore: Delete old NodeJS server code

--- old-src/db/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const redis = require("redis");
-const client = redis.createClient();
-client.connect();
-
-module.exports = client;
--- old-src/middleware/loadSourceIP.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-module.exports = function loadSourceIP(req, res, next) {
-    req.sourceIP = req.headers["cf-connecting-ip"] || req.socket.remoteAddress;
-    return next();
-}
